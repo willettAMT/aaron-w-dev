70217541c168598db3d8f616f0b1269c868c94ea
Aaron <willettamt@gmail.com>
2023-03-28 22:33:24 -0700

    app page intro changed

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/willettAMT" target="_blank" rel="noreferrer">My github</a>, Hello buddies!
+    Check out <a href="https://github.com/willettAMT" target="_blank" rel="noreferrer">my github</a>, friends!
   </p>
 
 </main>
