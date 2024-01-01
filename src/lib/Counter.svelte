620237e58abc06f3c99f71912069f2a9d4e4b1b6
Aaron <willettamt@gmail.com>
2023-12-31 20:09:34 -0800

    new button

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
+  const unclicked: string = "Click Me!";
   const increment = () => {
     count += 1
   }
 </script>
 
 <button on:click={increment}>
-  count is {count}
+  {count === 0 ? unclicked : count}
 </button>
