3870d7fd30d3a4636d6f4098272706877277ed25
Aaron <willettamt@gmail.com>
2023-12-31 18:55:27 -0800

    formatting and camera angle for animation changed

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 10 );
+const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 10 );
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
