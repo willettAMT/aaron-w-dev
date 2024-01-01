aac7005b6fd376f82d97f04845c31aa62981abd8
Aaron <willettamt@gmail.com>
2023-12-31 20:26:07 -0800

    header changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Counter from './lib/Counter.svelte'
 </script>
-<main>
+<div>
 
   <h1>aaron willett</h1>
   <h4><i>portfolio 0.0.2</i></h4>
@@ -9,10 +9,22 @@
   <div class="card">
     <Counter />
   </div>
+  <div class="card">
+    <p>Software engineer with experience creating developer-friendly products that improve efficiency of systems.<br>
+       My current pathway is targeting IoT + AI and the application within different industries. <br>
+       I am interested in secure data-aggregation at the hardware level, the research and development of <br>
+       DL models interpreting the raw data, securing wireless protocols, and the cloud infrastructure for optimizing device UX.<br><br>
 
+       Previously, I worked on an open source product, Svelvet, as a software engineer, where I conceptualized and developed a <br>
+       component library for building flow-charts on the Svelte compiler. <br>
+       Check out our documentation page (https://svelvet.io) for the playground!<br>
+    </p>
+  </div>
+<div class="card"></div>
   <p>
     Check out my <a href="https://github.com/willettAMT" target="_blank" rel="noreferrer">github</a>, friends!<br>
     Find me on <a href="https://linkedin.com/in/willettAMT" target="_blank" rel="noreferrer">LinkedIn</a>!
   </p>
+</div>
 
 </main>
