cc22e637b3876a9cf53e577b138bc9b620d65bca
aaronw <willett.amt@gmail.com>
2025-03-30 13:09:26 -0700

    generic commit

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -7,16 +7,15 @@ import Counter from './lib/Counter.svelte'
 
     <p>I am a software engineer passionate about building efficient, developer-friendly systems. My current focus is on IoT,<br>
         embedded systems, and low-level programming, with a keen interest in tackling the "30 million line problem" as <br>
-        described by Casey Muratori. I explore ways to make software more maintainable, performant, and secure—especially<br>
-        in resource-constrained environments.<br><br>
+        described by Casey Muratori. I explore ways to make software more maintainable, performant, and secure<br>
+        — especially in resource-constrained environments.<br><br>
         My current coursework includes embedded C, NASM, embedded Rust, Malware Reverse Engineering, and Web and Cloud Security.<br>
         I also contribute to firmware development with the Portland State Aerospace Society. My interests span secure data<br>
         aggregation at the hardware level, securing wireless protocols, and optimizing system efficiency from the ground up.<br>
         If you’d like to chat about systems programming, embedded security, or anything tech-related, feel free to reach out!<br><br>
 
         Previously, I worked on an open source product, Svelvet, as a software engineer, where I conceptualized and developed a <br>
-        component library for building flow-charts on the Svelte compiler. <br>
-        Check out our documentation page <a href="https://svelvet.io" target="_blank" rel="noreferrer"><i>Svelvet</i></a> for the playground!<br>
+        component library for building flow-charts on the Svelte compiler. Check out our documentation page <a href="https://svelvet.io" target="_blank" rel="noreferrer"><i>Svelvet</i></a> for the playground!<br>
     </p>
     <p>
         Check out my <a href="https://github.com/willettAMT" target="_blank" rel="noreferrer"><i>Github</i></a>, friends!<br>
