967b3ca855bd1b5b20946b94ac130027611aad5c
Aaron <willettamt@gmail.com>
2025-04-09 14:20:55 -0700

    generic commit

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
         embedded systems, and low-level programming, with a keen interest in tackling the "30 million line problem" as <br>
         described by Casey Muratori. I explore ways to make software more maintainable, performant, and secure<br>
         — especially in resource-constrained environments.<br><br>
-        My current coursework includes embedded C, NASM, embedded Rust, Malware Reverse Engineering, and Web and Cloud Security.<br>
+        My current coursework includes embedded C, NASM, embedded rust, malware reverse engineering, and web and cloud security.<br>
         I also contribute to firmware development with the Portland State Aerospace Society. My interests span secure data<br>
         aggregation at the hardware level, securing wireless protocols, and optimizing system efficiency from the ground up.<br>
         If you’d like to chat about systems programming, embedded security, or anything tech-related, feel free to reach out!<br><br>
