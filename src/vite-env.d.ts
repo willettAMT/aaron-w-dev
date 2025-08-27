03110e4bccfe43f9ff47464113e61b9622786b6b
aaronw <willett.amt@gmail.com>
2025-08-27 01:32:42 -0700

    generic commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
 // <reference types="svelte" />
 // <reference types="vite/client" />
+declare module '*.svelte' {
+  import type { ComponentType, SvelteComponent } from 'svelte'
+  const component: ComponentType<SvelteComponent>
+  export default component
+}
